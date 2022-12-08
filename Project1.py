a899872006a7ee58cbfd2b40c94638652b7283ef
GowshikNirubhan <118094698+GowshikNirubhan@users.noreply.github.com>
2022-12-08 01:26:30 -0500

    Update Project1.py

--- Project1.py
+++ Project1.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 class Project:
     
     def new_emplyee(self,url):
-        #admin credentials
+        #credentials
         usernameA = "Admin"
         passwordA = "admin123"
         firstname= input('Enter First Name:')
@@ -22,12 +22,12 @@ class Project:
         self.driver.get(url)
         time.sleep(4)
     
-        #credentials xpath
+        #admin credentials xpath
         usernameA_xpath = "/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input"
         passwordA_xpath = "/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input"
         login_button_xpath= "/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button"
 
-        #fill the crednetials   
+        #admin fill the crednetials   
         usernameA_xpath=self.driver.find_element(by=By.XPATH, value=usernameA_xpath)
         passwordA_xpath=self.driver.find_element(by=By.XPATH, value=passwordA_xpath)
         login_button_xpath=self.driver.find_element(by=By.XPATH, value=login_button_xpath)
